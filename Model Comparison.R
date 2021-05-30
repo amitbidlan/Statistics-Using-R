4eba08686d6e8c334e4a74d1b0ec5e07d2d9e0b6
amitbidlan <73678578+amitbidlan@users.noreply.github.com>
2021-05-31 01:24:41 +0530

    This is just a temp

---
+++ Model Comparison.R
@@ -0,0 +1,11 @@
+############ Model Comparisons #################
+
+# This Means that we are starting with simple model and adding more variables 
+# and compare.
+
+# A control variable is any variable that's held constant in a research study. 
+# basically putting their coefficients as 0.
+
+
+
+
