ba7c4b249b7d6eb728b5a78d2db45df11fb1af72
amitbidlan <73678578+amitbidlan@users.noreply.github.com>
2021-05-31 20:36:56 +0530

    Supression In Regression

---
+++ ASRM-Unit-3-Lec3.R
@@ -0,0 +1,11 @@
+
+############### Supression In Regression ##################
+
+# Suppressions can be defined as “a variable which increases the predictive validity 
+# of another variable (or set of variables) by its inclusion in a regression equation,” 
+# a suppression effect would be present when the direct and indirect effects of an independent 
+# variable on a dependent variable have opposite signs.
+
+
+# the expectation from adding any new variable to the regression equation is to get higher R, 
+# but if R decrease, then this is called suppression.
